38067bc4bf3f8a6a0125417cec53404a27d7651e
Valentin Kelbakh <valentinkelbax@gmail.com>
2023-03-18 02:04:39 +0600

    added routers to urls

--- config/urls.py
+++ config/urls.py
@@ -29,6 +29,9 @@ from home import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'items', views.ItemViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
