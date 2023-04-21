c39e74d2d9aa295384a02054c1271f358978d9bb
Valentin Kelbakh <valentinkelbax@gmail.com>
2023-04-21 13:47:55 +0600

    added appropriate paths/registered routers for viewsets

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
 from django.urls import include, path
 from rest_framework import routers
 from home import views
+
+
 router = routers.DefaultRouter()
 # router.register(r'users', views.UserViewSet)
 # router.register(r'groups', views.GroupViewSet)
@@ -34,11 +36,16 @@ router.register(r'roles', views.RoleViewSet)
 router.register(r'profiles', views.ProfileViewSet)
 router.register(r'categories', views.CategoryViewSet)
 router.register(r'products', views.ProductViewSet)
+router.register(r'receipts', views.ReceiptViewSet)
+router.register(r'receipt_products', views.ReceiptProductViewSet)
+router.register(r'inventory', views.InventoryViewSet)
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('actions/', views.ActionsView.as_view()),
+    path('subjects/', views.SubjectsView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
