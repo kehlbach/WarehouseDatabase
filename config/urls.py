312596b33d59d9d0f77964736d5da7fd6ecdccb9
Valentin Kelbakh <valentinkelbax@gmail.com>
2023-05-11 22:29:46 +0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from home import views
 
 router = routers.DefaultRouter()
 router.register(r'departments', views.DepartmentViewSet)
-router.register(r'permissions', views.PermissionViewSet)
+router.register(r'role_permissions', views.RolePermissionViewSet)
 router.register(r'roles', views.RoleViewSet)
 router.register(r'profiles', views.ProfileViewSet)
 router.register(r'categories', views.CategoryViewSet)
@@ -13,10 +13,11 @@ router.register(r'products', views.ProductViewSet)
 router.register(r'receipts', views.ReceiptViewSet)
 router.register(r'receipt_products', views.ReceiptProductViewSet)
 router.register(r'inventory', views.InventoryViewSet)
+router.register(r'latest_inventory', views.InventorySummaryViewSet, basename='latest_inventory')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('actions/', views.ActionsView.as_view()),
     path('subjects/', views.SubjectsView.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
